96aba43105b313b2ac1ffa478f2062e3a1cc2379
Melina Garza <mjg2290@columbia.edu>
2023-06-18 11:35:29 -0400

    registered the models

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Score
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display=('id','title','composer')
 
 # Register your models here.
+admin.site.register(Score,ScoreAdmin)
